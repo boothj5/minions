758172a85f6751a7ff54e420826ccc9bb33b160f
James Booth <boothj5@gmail.com>
2015-02-17 00:58:36 +0000

    Added digest minion

---
+++ minions-contrib/digest-minion/src/main/java/com/boothj5/minions/digest/DigestMinion.java
@@ -0,0 +1,52 @@
+package com.boothj5.minions.digest;
+
+import com.boothj5.minions.api.Minion;
+import com.boothj5.minions.api.MinionsException;
+import com.boothj5.minions.api.MinionsRoom;
+import org.apache.commons.codec.binary.Base64;
+import org.apache.commons.codec.digest.DigestUtils;
+import org.apache.commons.lang3.StringUtils;
+
+import java.nio.charset.StandardCharsets;
+
+public class DigestMinion implements Minion {
+    private final String COMMAND = "digest";
+
+    @Override
+    public String getCommand() {
+        return COMMAND;
+    }
+
+    @Override
+    public String getHelp() {
+        return COMMAND + " - Calculate various digests of a given value. Send 'help' for more information.";
+    }
+
+    @Override
+    public void onMessage(MinionsRoom muc, String from, String message) throws MinionsException {
+        try {
+            String[] tokens = StringUtils.split(message, " ");
+            String value;
+            String result;
+            switch (tokens[1]) {
+                case "help":
+                    String help =
+                        "\n" + "sha1 value - Compute sha1 hash of value, returning the result as a hex string." +
+                        "\n" + "sha1base64 value - Compute sha1 hash and base64 encode the result.";
+
+                    muc.sendMessage(help);
+                    break;
+                case "sha1":
+                    result = DigestUtils.shaHex(message.substring(13).getBytes(StandardCharsets.UTF_8));
+                    muc.sendMessage(from + ": " + result);
+                    break;
+                case "sha1base64":
+                    result = new String(Base64.encodeBase64(DigestUtils.sha(message.substring(19).getBytes(StandardCharsets.UTF_8))));
+                    muc.sendMessage(from + ": " + result);
+                    break;
+            }
+        } catch (Throwable t) {
+            t.printStackTrace();
+        }
+    }
+}
