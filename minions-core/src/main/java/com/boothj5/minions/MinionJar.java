c553a18905841a846b2ba54a6a6e4bb08efbb789
James Booth <boothj5@gmail.com>
2015-02-20 22:47:58 +0000

    Fixed timestamp checking for updates

--- minions-core/src/main/java/com/boothj5/minions/MinionJar.java
+++ minions-core/src/main/java/com/boothj5/minions/MinionJar.java
@@ -6,7 +6,6 @@ import java.io.IOException;
 import java.io.InputStream;
 import java.lang.reflect.Constructor;
 import java.lang.reflect.InvocationTargetException;
-import java.net.MalformedURLException;
 import java.net.URL;
 import java.net.URLClassLoader;
 import java.util.jar.JarInputStream;
@@ -16,37 +15,43 @@ public class MinionJar {
     public static final String MANIFEST_CLASS = "MinionClass";
     private static final String MANIFEST_COMMAND = "MinionCommand" ;
 
-    private final File file;
-    private final Manifest manifest;
+    private final String name;
+    private final long timestamp;
+    private final URL url;
+    private final String command;
+    private final String className;
 
     public MinionJar(File file) throws IOException {
-        this.file = file;
+        name = file.getName();
+        timestamp = file.lastModified();
+        url = file.toURI().toURL();
         InputStream in = new FileInputStream(file);
         JarInputStream stream = new JarInputStream(in);
-        this.manifest = stream.getManifest();
+        Manifest manifest = stream.getManifest();
+        command = manifest.getMainAttributes().getValue(MANIFEST_COMMAND);
+        className = manifest.getMainAttributes().getValue(MANIFEST_CLASS);
         stream.close();
         in.close();
     }
 
     public String getName() {
-        return file.getName();
+        return name;
     }
 
     public Long getTimestamp() {
-        return file.lastModified();
+        return timestamp;
     }
 
-    public URL getURL() throws MalformedURLException {
-        return file.toURI().toURL();
+    public URL getURL() {
+        return url;
     }
 
     public String getCommand() {
-        return manifest.getMainAttributes().getValue(MANIFEST_COMMAND);
+        return command;
     }
 
     public Minion loadMinionClass(URLClassLoader loader)
             throws ClassNotFoundException, NoSuchMethodException, IllegalAccessException, InvocationTargetException, InstantiationException {
-        String className = manifest.getMainAttributes().getValue(MANIFEST_CLASS);
         Class<?> clazz = Class.forName(className, true, loader);
         Class<? extends Minion> minionClazz = clazz.asSubclass(Minion.class);
         Constructor<? extends Minion> ctr = minionClazz.getConstructor();
