69d3d150d86e838986779934d30271d82edc5693
Adam Dougal <barmybuckle@gmail.com>
2015-03-10 13:00:24 +0000

    Added ascii minion

---
+++ minions-contrib/ascii-minion/src/main/java/com/boothj5/minions/ascii/AsciiMinion.java
@@ -0,0 +1,45 @@
+package com.boothj5.minions.ascii;
+
+import com.boothj5.minions.Minion;
+import com.boothj5.minions.MinionsException;
+import com.boothj5.minions.MinionsRoom;
+
+import java.awt.*;
+import java.awt.image.BufferedImage;
+
+public class AsciiMinion extends Minion {
+
+    @Override
+    public String getHelp() {
+        return writeMessage("HELP");
+    }
+
+    @Override
+    public void onMessage(MinionsRoom minionsRoom, String from, String message) throws MinionsException {
+        String response = writeMessage(message);
+        minionsRoom.sendMessage(response);
+    }
+
+    private String writeMessage(String message) {
+        BufferedImage image = new BufferedImage(144, 32, BufferedImage.TYPE_INT_RGB);
+        Graphics g = image.getGraphics();
+        g.setFont(new Font("Dialog", Font.PLAIN, 16));
+        Graphics2D graphics = (Graphics2D) g;
+        graphics.setRenderingHint(RenderingHints.KEY_TEXT_ANTIALIASING,
+                RenderingHints.VALUE_TEXT_ANTIALIAS_ON);
+        graphics.drawString(message, 6, 24);
+
+        StringBuilder sbMain = new StringBuilder();
+        for (int y = 0; y < 32; y++) {
+            StringBuilder sb = new StringBuilder();
+            for (int x = 0; x < 144; x++) {
+                sb.append(image.getRGB(x, y) == -16777216 ? " " : image.getRGB(x, y) == -1 ? "#" : "*");
+            }
+            if (sb.toString().trim().isEmpty()) {
+                continue;
+            }
+            sbMain.append(sb.toString()).append("\n");
+        }
+        return sbMain.toString();
+    }
+}
