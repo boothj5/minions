1850b303b8e7874b9eb2e2ee6d86dd6ad2b05082
James Booth <boothj5@gmail.com>
2015-04-13 00:25:31 +0100

    Added more hashes to digest minion

--- minions-contrib/digest-minion/src/main/java/com/boothj5/minions/digest/DigestMinion.java
+++ minions-contrib/digest-minion/src/main/java/com/boothj5/minions/digest/DigestMinion.java
@@ -13,6 +13,12 @@ public class DigestMinion extends Minion {
     public static final String HELP = "help";
     public static final String SHA1 = "sha1";
     public static final String SHA1BASE64 = "sha1base64";
+    public static final String SHA256 = "sha256";
+    public static final String SHA256BASE64 = "sha256base64";
+    public static final String SHA512 = "sha512";
+    public static final String SHA512BASE64 = "sha512base64";
+    public static final String MD5 = "md5";
+    public static final String MD5BASE64 = "md5base64";
 
     @Override
     public String getHelp() {
@@ -23,29 +29,86 @@ public class DigestMinion extends Minion {
     public void onMessage(MinionsRoom muc, String from, String message) throws MinionsException {
         String[] tokens = StringUtils.split(message, " ");
         String command = tokens[0];
-        String result;
+
+        int beginIndex;
+        byte[] bytes, hash, base64;
+        String result, substring;
+
         switch (command) {
             case HELP:
                 String help =
                     "\n" + "sha1 value - Compute sha1 hash of value, returning the result as a hex string." +
-                    "\n" + "sha1base64 value - Compute sha1 hash and base64 encode the result.";
-
+                    "\n" + "sha1base64 value - Compute sha1 hash and base64 encode the result." +
+                    "\n" + "sha256 value - Compute sha256 hash of value, returning the result as a hex string." +
+                    "\n" + "sha256base64 value - Compute sha256 hash and base64 encode the result." +
+                    "\n" + "sha512 value - Compute sha512 hash of value, returning the result as a hex string." +
+                    "\n" + "sha512base64 value - Compute sha512 hash and base64 encode the result." +
+                    "\n" + "md5 value - Compute md5 hash of value, returning the result as a hex string." +
+                    "\n" + "md5base64 value - Compute md5 hash and base64 encode the result.";
                 muc.sendMessage(help);
                 break;
             case SHA1:
-                int beginIndex = SHA1.length() + 1;
-                String substring = message.substring(beginIndex);
-                byte[] bytes = substring.getBytes(StandardCharsets.UTF_8);
+                beginIndex = SHA1.length() + 1;
+                substring = message.substring(beginIndex);
+                bytes = substring.getBytes(StandardCharsets.UTF_8);
                 result = DigestUtils.shaHex(bytes);
                 muc.sendMessage(from + ": " + result);
                 break;
             case SHA1BASE64:
-                int beginIndex1 = SHA1BASE64.length() + 1;
-                String substring1 = message.substring(beginIndex1);
-                byte[] bytes1 = substring1.getBytes(StandardCharsets.UTF_8);
-                byte[] sha = DigestUtils.sha(bytes1);
-                byte[] bytes2 = Base64.encodeBase64(sha);
-                result = new String(bytes2);
+                beginIndex = SHA1BASE64.length() + 1;
+                substring = message.substring(beginIndex);
+                bytes = substring.getBytes(StandardCharsets.UTF_8);
+                hash = DigestUtils.sha(bytes);
+                base64 = Base64.encodeBase64(hash);
+                result = new String(base64);
+                muc.sendMessage(from + ": " + result);
+                break;
+            case SHA256:
+                beginIndex = SHA256.length() + 1;
+                substring = message.substring(beginIndex);
+                bytes = substring.getBytes(StandardCharsets.UTF_8);
+                result = DigestUtils.sha256Hex(bytes);
+                muc.sendMessage(from + ": " + result);
+                break;
+            case SHA256BASE64:
+                beginIndex = SHA256BASE64.length() + 1;
+                substring = message.substring(beginIndex);
+                bytes = substring.getBytes(StandardCharsets.UTF_8);
+                hash = DigestUtils.sha256(bytes);
+                base64 = Base64.encodeBase64(hash);
+                result = new String(base64);
+                muc.sendMessage(from + ": " + result);
+                break;
+            case SHA512:
+                beginIndex = SHA512.length() + 1;
+                substring = message.substring(beginIndex);
+                bytes = substring.getBytes(StandardCharsets.UTF_8);
+                result = DigestUtils.sha512Hex(bytes);
+                muc.sendMessage(from + ": " + result);
+                break;
+            case SHA512BASE64:
+                beginIndex = SHA512BASE64.length() + 1;
+                substring = message.substring(beginIndex);
+                bytes = substring.getBytes(StandardCharsets.UTF_8);
+                hash = DigestUtils.sha512(bytes);
+                base64 = Base64.encodeBase64(hash);
+                result = new String(base64);
+                muc.sendMessage(from + ": " + result);
+                break;
+            case MD5:
+                beginIndex = MD5.length() + 1;
+                substring = message.substring(beginIndex);
+                bytes = substring.getBytes(StandardCharsets.UTF_8);
+                result = DigestUtils.md5Hex(bytes);
+                muc.sendMessage(from + ": " + result);
+                break;
+            case MD5BASE64:
+                beginIndex = MD5BASE64.length() + 1;
+                substring = message.substring(beginIndex);
+                bytes = substring.getBytes(StandardCharsets.UTF_8);
+                hash = DigestUtils.md5(bytes);
+                base64 = Base64.encodeBase64(hash);
+                result = new String(base64);
                 muc.sendMessage(from + ": " + result);
                 break;
         }
