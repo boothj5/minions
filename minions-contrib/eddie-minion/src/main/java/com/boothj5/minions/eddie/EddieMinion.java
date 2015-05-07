3131c1ef474e6fa0888314a1b8d73ade73c64422
James Booth <boothj5@gmail.com>
2015-05-07 13:27:31 +0100

    Eddie minion replies to all messages

--- minions-contrib/eddie-minion/src/main/java/com/boothj5/minions/eddie/EddieMinion.java
+++ minions-contrib/eddie-minion/src/main/java/com/boothj5/minions/eddie/EddieMinion.java
@@ -14,11 +14,16 @@ public class EddieMinion extends Minion {
     public void onMessage(MinionsRoom muc, String from, String message) throws MinionsException {
         if (message.contains("\\m/")) {
             muc.sendMessage("\\m/");
-        } else if (message.contains("/m\\")
+        }
+
+        if (message.contains("/m\\")
                 || message.contains("\\m\\")
                 || message.contains("/m/")) {
             muc.sendMessage(from + ", it's \\m/");
-        } else if (message.toLowerCase().contains("eddie")) {
+
+        }
+
+        if (message.toLowerCase().contains("eddie")) {
             muc.sendMessage("http://maiden-world.com/images/wallpaper/Iron_Maiden_032_1.jpg");
         }
     }
